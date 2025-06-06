a600ad8ce0e452b2ada87dc181ab5e145b77fdf0
Falcon153rave <fal143con@gmail.com>
2025-06-06 21:48:20 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'D~Falcon Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "D~Falcon Bot",
+  botOwner: 'D~Falcon Bot', // Your name
+  ownerNumber: '2348143066070', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
